152ae0ccdffa30aa2c987a664b689e6d6be8ed2a
John Hetterich <hettjohn@badinhs.org>
2017-01-03 22:59:14 -0500

    second commit

--- Midterm Badin App/Midterm Badin App/ViewController.swift
+++ Midterm Badin App/Midterm Badin App/ViewController.swift
@@ -8,39 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class ViewController: UIViewController {
 
-    @IBOutlet weak var classesTableView: UITableView!
+    @IBOutlet weak var homeToEventsButton: UIButton!
+    @IBOutlet weak var tapMeCounterLabel: UILabel!
+    @IBOutlet weak var tapMeLabel: UILabel!
+    var tapCounter = 0
+    @IBAction func tapMeButton (_ sender: AnyObject) {
+        tapCounter = tapCounter + 1
+        tapMeCounterLabel.text = "\(tapCounter)"
+        //This is a conditional
+        if tapCounter == 10 {
+            tapMeCounterLabel.text = "\(tapCounter) SLOW DOWN!"
+            print("Count has reached 10")
+        }
+        //This is a conditional
+        if tapCounter == 50 {
+            tapMeCounterLabel.text = "\(tapCounter) Good Job! Keep Going"
+            print("Count has reached 50")
+        }
+        //This is a conditional
+        if tapCounter == 55 {
+            tapMeCounterLabel.text = "\(tapCounter) Good Job! Keep Going"
+            print("Count has reached 55")
+            tapCounter = tapCounter - 40
+        }
 
-   var classes = ["Math", "English", "Science"]
+    }
+    //This is an array
+   var classesList = ["Math", "English", "Science"]
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        classesTableView.dataSource = self //used to define the table
-        classesTableView.delegate = self //used to define contents
-    }
 
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        //number of rows
-        return classes.count
-    }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        //what do you want in the cells
-        let cell = UITableViewCell()
-        cell.textLabel?.text = classes[indexPath.row]
-        return cell
-    }
-
-
-
 }
-
--- MidtermPlayground.playground/Contents.swift
+++ MidtermPlayground.playground/Contents.swift
@@ -3,3 +3,45 @@
 import UIKit
 
 var str = "Hello, playground"
+
+var name = "Class"
+if name == "John" {
+    print("You're pretty cool")
+} else {
+    print("Sorry for your loss")
+}
+
+func addTwo (num1 : Int, num2 : Int) {
+    print(num1 + num2)
+}
+
+addTwo(num1: 5, num2: 10)
+addTwo(num1: 8, num2: 2)
+
+class SchoolClass {
+    var name = " "
+    var period = 0
+    var level = " "
+    
+}
+
+
+
+var SchoolClass1 = SchoolClass()
+SchoolClass1.name = "Math"
+SchoolClass1.period = 8
+SchoolClass1.level = "AP"
+
+var SchoolClass2 = SchoolClass()
+SchoolClass2.name = "English"
+SchoolClass2.period = 3
+SchoolClass2.level = "ACP"
+
+print(SchoolClass1.name)
+print(SchoolClass2.name)
+
+func info () {
+    print("This is " + SchoolClass1.name + " class")
+}
+
+info()
